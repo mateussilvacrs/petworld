a242dc0088c6419c615d7cce263ba4cc17c5fb3d
Mateus <mateus_silva139@hotmail.com>
2026-02-16 15:40:36 -0300

    menu-mobile fix

---
+++ index.js
@@ -0,0 +1,9 @@
+const menumobile = document.querySelector(".menu-mobile");
+const buttonsmobile = document.querySelector(".buttons-mobile");
+console.log(menumobile)
+
+
+  menumobile.addEventListener('click', () => {
+    buttonsmobile.classList.toggle("active")
+    console.log("ativo")
+  });
