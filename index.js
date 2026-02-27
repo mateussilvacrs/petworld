0d47c1640c8645d9a6c66bfc7062a6fe18d90c5e
Mateus <mateus_silva139@hotmail.com>
2026-02-27 12:40:33 -0300

    remove console.log

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const menumobile = document.querySelector(".menu-mobile");
 const buttonsmobile = document.querySelector(".buttons-mobile");
-console.log(menumobile)
+
 
 
   menumobile.addEventListener('click', () => {
